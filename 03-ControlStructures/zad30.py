3264fc972f910282f2b0e8c3435ad4d8e8a43e54
nattcze <nzczech@gmail.com>
2022-11-03 20:10:31 +0100

    made changes

--- 03-ControlStructures/zad30.py
+++ 03-ControlStructures/zad30.py
@@ -4,5 +4,26 @@
 # Using loop statements check that the number N is divisible only by 1 and by N.
 # Prime numbers: 2 3 5 7 11 …
 
+# n = int(input("Enter number of prime numbers: "))
+
+# for i in range(2, n+1):
+#     if n > 1 :
+
+#         for i in range(2, n+1):
+#             if n%i == 0:
+#                 break
+#         else:
+#             print(n)
 
 
+# l = int(input("beggining of the interval "))
+# u = int(input("end of the interval "))
+
+# for num in range(l, u+1):
+#     if num > 1:
+
+#         for i in range(2,num):
+#             if num%1 == 0:
+#                 break
+#         else:
+#             print(num, end="")
